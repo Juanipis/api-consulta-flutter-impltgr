6670d63207ce9caacf5a68304859e1ea50bb89db
Juan Pablo Díaz Correa <juanipis@gmail.com>
2023-11-06 12:49:35 -0500

    done character get id fastapi

--- music_api/lib/presentation/screens/User/id_character_user.dart
+++ music_api/lib/presentation/screens/User/id_character_user.dart
@@ -1,10 +1,14 @@
 import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mime/mime.dart';
 import 'package:music_api/blocs/character_user/character_user_blocs.dart';
 import 'package:music_api/blocs/character_user/character_user_events.dart';
 import 'package:music_api/blocs/character_user/character_user_states.dart';
-
+import 'package:music_api/repositories/character_user.dart';
 class IDCharacterUser extends StatefulWidget {
   const IDCharacterUser({super.key});
 
@@ -15,7 +19,7 @@ class IDCharacterUser extends StatefulWidget {
 class _IDCharacterUserState extends State<IDCharacterUser> {
   String uuid = '';
   final TextEditingController _controller = TextEditingController();
-  
+
 
   @override
   Widget build(BuildContext context) {
@@ -49,56 +53,116 @@ class _IDCharacterUserState extends State<IDCharacterUser> {
               ),
               const SizedBox(height: 20),
               BlocBuilder<CharacterUserBloc, CharacterUserState>(
-                builder: (context, state) {
-                  if (state is CharacterUserLoadingState) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-                  if (state is CharacterUserLoadedState) {
-                    final characterUser = state.character;
-                    final images = characterUser.data.entries
-                        .map((entry) {
-                          final bytes = base64Decode(entry.value);
-                          return Image.memory(bytes, fit: BoxFit.cover);
-                        })
-                        .toList();
+              builder: (context, state) {
+                if (state is CharacterUserLoadingState) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                if (state is CharacterUserLoadedState) {
+                  final characterUser = state.character;
+                  final images = characterUser.data.entries
+                      .map((entry) {
+                        final bytes = base64Decode(entry.value);
+                        return Image.memory(bytes, fit: BoxFit.cover);
+                      })
+                      .toList();
 
-                    return Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text('UUID: ${characterUser.uuid}'),
-                        Flexible(
-                          child: GridView.builder(
-                            shrinkWrap: true,
-                            physics: const NeverScrollableScrollPhysics(), // Deshabilitar el scroll en GridView
-                            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: MediaQuery.of(context).size.width > 600 ? 3 : 2,
-                              childAspectRatio: 1,
-                              crossAxisSpacing: 4,
-                              mainAxisSpacing: 4,
-                            ),
-                            itemCount: images.length,
-                            itemBuilder: (context, index) {
-                              return Card(
-                                clipBehavior: Clip.antiAlias,
-                                child: Container(
-                                  constraints: BoxConstraints(
-                                    maxHeight: 150, // Tamaño máximo para las imágenes
-                                  ),
-                                  child: images[index],
-                                ),
-                              );
-                            },
-                          ),
+                  // Añadimos un nuevo Widget al final de la lista de Widgets que ya existen
+                  List<Widget> columnChildren = [
+                    Text('UUID: ${characterUser.uuid}'),
+                    Flexible(
+                      child: GridView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: MediaQuery.of(context).size.width > 600 ? 3 : 2,
+                          childAspectRatio: 1,
+                          crossAxisSpacing: 4,
+                          mainAxisSpacing: 4,
                         ),
-                      ],
-                    );
-                  }
-                  if (state is CharacterUserErrorState) {
-                    return Center(child: Text(state.message));
-                  }
-                  return const Center(child: Text('Introduzca un UUID para buscar'));
-                },
-              ),
+                        itemCount: images.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            clipBehavior: Clip.antiAlias,
+                            child: Container(
+                              constraints:  const BoxConstraints(
+                                maxHeight: 150,
+                              ),
+                              child: images[index],
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    // Botón que se muestra solo si el estado es CharacterUserLoadedState
+                    ElevatedButton(
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: Text('Desea agregar más imagenes al personaje con el uuid ${characterUser.uuid}'),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text('Cancelar'),
+                                ),
+                                TextButton(
+                                  onPressed: () async {
+
+                                    // Trigger file selection
+                                    FilePickerResult? result = await FilePicker.platform.pickFiles(type: FileType.image);
+
+                                    // If the user picked an image
+                                    if (result != null) {
+                                      Uint8List fileBytes = result.files.first.bytes!;
+                                      String fileName = result.files.first.name;
+
+                                      CharacterUserRepository repository = CharacterUserRepository();
+
+                                      try {
+                                        // Call the method to upload the image bytes
+                                        var uploadResult = await repository.insertNewCharacterImage(characterUser.uuid, fileBytes, fileName);
+                                        // Show a success message or update the UI accordingly
+                                        ScaffoldMessenger.of(context).showSnackBar(
+                                          SnackBar(content: Text('Image uploaded successfully: $uploadResult')),
+                                        );
+                                      } catch (e) {
+                                        // If the upload fails, show an error message
+                                        ScaffoldMessenger.of(context).showSnackBar(
+                                          SnackBar(content: Text('Failed to upload image: $e')),
+                                        );
+                                      }
+                                    } else {
+                                      // User canceled the picker
+                                      ScaffoldMessenger.of(context).showSnackBar(
+                                        SnackBar(content: Text('No image selected')),
+                                      );
+                                    }
+                                  },
+                                  child: const Text('Aceptar'),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                      child: const Text('Mostrar UUID'),
+                    ),
+                  ];
+
+                  return Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: columnChildren,
+                  );
+                }
+                if (state is CharacterUserErrorState) {
+                  return Center(child: Text(state.message));
+                }
+                return const Center(child: Text('Introduzca un UUID para buscar'));
+              },
+            ),
             ],
           ),
         ),
@@ -106,4 +170,5 @@ class _IDCharacterUserState extends State<IDCharacterUser> {
     );
     
   }
+  
 }
--- music_api/lib/repositories/character_user.dart
+++ music_api/lib/repositories/character_user.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:http/http.dart' as http;
 import 'package:music_api/models/character_user.dart';
-
+import 'package:path/path.dart';
 class CharacterUserRepository{
   Future<CharacterUser> getUserCharacters(String uuid) async {
     Uri uri = Uri.parse('http://127.0.0.1:8000/character/get_character_images_bytes/$uuid');
@@ -14,4 +16,32 @@ class CharacterUserRepository{
       throw Exception('Failed to load user characters');
     }
   }
+
+  Future<Map<String, dynamic>> insertNewCharacterImage(String characterUuid, Uint8List fileBytes, String fileName) async {
+  var uri = Uri.parse('http://127.0.0.1:8000/character/insert_new_character_image');
+  uri = uri.replace(queryParameters: {
+    ...uri.queryParameters,
+    'character_uuid': characterUuid, // Asegúrate de que esto no sea null.
+  });
+  
+  var request = http.MultipartRequest("POST", uri);
+  request.files.add(http.MultipartFile.fromBytes('character_image', fileBytes, filename: fileName));
+
+  var response = await request.send();
+
+  response.stream.transform(utf8.decoder).listen((value) {
+  print(value); // Here you will see the detailed error message.
+  });
+
+
+  if (response.statusCode == 200) {
+    var responseData = await response.stream.toBytes();
+    var responseString = String.fromCharCodes(responseData);
+    return {'inserted': true, 'data': responseString};
+  } else {
+    throw Exception('Failed to upload image: Server responded with status code ${response.statusCode}');
+  }
 }
+
+}
+
